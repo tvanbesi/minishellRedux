726ca41a7d252ec7a702b43bb1c2a76bf03a4b74
Thomas Vanbesien <tvanbesi@student.42.fr>
2020-12-21 17:55:50 +0100

    correct exit status for failing builtin

--- execute.c
+++ execute.c
@@ -6,7 +6,7 @@
 /*   By: tvanbesi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/13 12:41:35 by tvanbesi          #+#    #+#             */
-/*   Updated: 2020/12/21 13:37:07 by tvanbesi         ###   ########.fr       */
+/*   Updated: 2020/12/21 17:54:49 by tvanbesi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -96,6 +96,8 @@ void
 	{
 		if ((builtinret = builtin(command, shell)) == -1)
 			puterror(strerror(errno));
+		else if (builtinret == 1)
+			g_exitstatus = 1;
 		else if (builtinret == -2)
 		{
 			if (!(pathenv = findenv(shell->env, "PATH")))
