0275d016ffe06276ef6c1d4eec891816e11a6137
tvanbesi <tvanbesi@gmail.com>
2021-01-12 14:14:03 +0100

    pipe.c normed

--- pipe.c
+++ pipe.c
@@ -6,86 +6,112 @@
 /*   By: tvanbesi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/14 18:27:26 by tvanbesi          #+#    #+#             */
-/*   Updated: 2020/12/22 09:26:27 by tvanbesi         ###   ########.fr       */
+/*   Updated: 2021/01/12 14:13:31 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static void
+	minipipechild(int pipeend, int fd[2], t_list *command, t_shell *shell)
+{
+	int		cmdsanity;
+	int		commandtype;
+
+	if ((cmdsanity = commandsanity(command, shell)) == -1)
+		exit(FATAL);
+	if (!iserror(cmdsanity) && !pipeend)
+		dup2(fd[1], STDOUT);
+	close(fd[0]);
+	close(fd[1]);
+	if (iserror(cmdsanity) && pipeend)
+		puterrorcmd(command, cmdsanity);
+	if (!iserror(cmdsanity))
+		execute(command, shell, cmdsanity);
+	else
+		exit(cmdsanity);
+	exit(g_exitstatus);
+}
+
+static void
+	minipipeflowbytype(int commandtype, t_list *command, t_shell *shell)
+{
+	if (commandtype == PIPE)
+	{
+		if (minipipe(command->next, shell, 0) == -1)
+			puterror(strerror(errno));
+	}
+	else if (commandtype > REDIRECTION)
+	{
+		if (redirect(command->next, shell) == -1)
+		{
+			puterror(strerror(errno));
+			g_exitstatus = EXIT_STAT_FAIL;
+		}
+	}
+	else if (commandtype == SIMPLE)
+	{
+		if (minipipe(command->next, shell, 1) == -1)
+			puterror(strerror(errno));
+	}
+}
+
+static int
+	minipipeflow(int stat_loc, int fd[2], t_list *command, t_shell *shell)
+{
+	int		cmdsanity;
+	int		commandtype;
+
+	commandtype = getcommandtype(command->next);
+	if ((cmdsanity = commandsanity(command->next, shell)) == -1)
+		return (-1);
+	if (!iserror(WEXITSTATUS(stat_loc))
+	&& (!iserror(cmdsanity) && cmdsanity != EMPTY))
+		dup2(fd[0], STDIN);
+	close(fd[0]);
+	close(fd[1]);
+	minipipeflowbytype(commandtype, command, shell);
+	if (iserror(WEXITSTATUS(stat_loc)))
+		puterrorcmd(command, WEXITSTATUS(stat_loc));
+	return (0);
+}
+
+static void
+	exitstatpipeend(int stat_loc)
+{
+	if (WEXITSTATUS(stat_loc) == NOCMD)
+		g_exitstatus = EXIT_STAT_NOCMD;
+	else if (WEXITSTATUS(stat_loc) == NOEXEC || WEXITSTATUS(stat_loc) == ISDIR)
+		g_exitstatus = EXIT_STAT_NOEXEC;
+	else if (!iserror(WEXITSTATUS(stat_loc)))
+		g_exitstatus = EXIT_STAT_SUCCESS;
+	else
+		g_exitstatus = EXIT_STAT_FAIL;
+}
 
 int
 	minipipe(t_list *command, t_shell *shell, int pipeend)
 {
 	int		fd[2];
 	int		stat_loc;
-	int		cmdsanity;
-	int		commandtype;
 
 	if (pipe(fd) == -1)
 		return (-1);
 	if ((g_pid = fork()) == -1)
 		return (-1);
 	if (g_pid == 0)
-	{
-		if ((cmdsanity = commandsanity(command, shell)) == -1)
-			exit(FATAL);
-		if (!iserror(cmdsanity) && !pipeend)
-			dup2(fd[1], STDOUT);
-		close(fd[0]);
-		close(fd[1]);
-		if (iserror(cmdsanity) && pipeend)
-			puterrorcmd(command, cmdsanity);
-		if (!iserror(cmdsanity))
-			execute(command, shell, cmdsanity);
-		else
-			exit(cmdsanity);
-		exit(g_exitstatus);
-	}
+		minipipechild(pipeend, fd, command, shell);
 	else
 	{
 		if (waitpid(g_pid, &stat_loc, 0) != g_pid)
 			return (-1);
 		if (WIFEXITED(stat_loc) && !pipeend)
 		{
-			commandtype = getcommandtype(command->next);
-			if ((cmdsanity = commandsanity(command->next, shell)) == -1)
+			if (minipipeflow(stat_loc, fd, command, shell) == -1)
 				return (-1);
-			if (!iserror(WEXITSTATUS(stat_loc)) && (!iserror(cmdsanity) && cmdsanity != EMPTY))
-				dup2(fd[0], STDIN);
-			close(fd[0]);
-			close(fd[1]);
-			if (commandtype == PIPE)
-			{
-				if (minipipe(command->next, shell, 0) == -1)
-					puterror(strerror(errno));
-			}
-			else if (commandtype > REDIRECTION)
-			{
-				if (redirect(command->next, shell) == -1)
-				{
-					puterror(strerror(errno));
-					g_exitstatus = EXIT_STAT_FAIL;
-				}
-			}
-			else if (commandtype == SIMPLE)
-			{
-				if (minipipe(command->next, shell, 1) == -1)
-					puterror(strerror(errno));
-			}
-			if (iserror(WEXITSTATUS(stat_loc)))
-				puterrorcmd(command, WEXITSTATUS(stat_loc));
 		}
 		else if (WIFEXITED(stat_loc) && pipeend)
-		{
-			if (WEXITSTATUS(stat_loc) == NOCMD)
-				g_exitstatus = EXIT_STAT_NOCMD;
-			else if (WEXITSTATUS(stat_loc) == NOEXEC || WEXITSTATUS(stat_loc) == ISDIR)
-				g_exitstatus = EXIT_STAT_NOEXEC;
-			else if (!iserror(WEXITSTATUS(stat_loc)))
-				g_exitstatus = EXIT_STAT_SUCCESS;
-			else
-				g_exitstatus = EXIT_STAT_FAIL;
-
-		}
+			exitstatpipeend(stat_loc);
 		else if (WIFSIGNALED(stat_loc))
 			printf("Signal received in pipe\n");
 	}
