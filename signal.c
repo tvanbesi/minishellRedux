50090d96ec222b1b9af68af58ca92210b5362bdc
tvanbesi <tvanbesi@gmail.com>
2021-03-09 12:33:47 +0100

    signal fix

--- signal.c
+++ signal.c
@@ -6,7 +6,7 @@
 /*   By: tvanbesi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/16 16:08:27 by tvanbesi          #+#    #+#             */
-/*   Updated: 2021/02/28 14:25:16 by user42           ###   ########.fr       */
+/*   Updated: 2021/03/09 04:07:12 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,10 @@ void
 {
 	(void)n;
 	if (g_pid == 0)
-		exit(BREAK);
+	{
+		write(STDERR, "\n> ", 3);
+		g_exitstatus = 128 + SIGINT;
+	}
 	else
 		kill(g_pid, SIGINT);
 }
@@ -27,7 +30,10 @@ void
 {
 	(void)n;
 	if (g_pid == 0)
-		write(STDERR, "\b \b", 3);
+	{
+		write(STDERR, "\b\b  \b\b", 6);
+		g_exitstatus = 128 + SIGQUIT;
+	}
 	else
 		kill(g_pid, SIGQUIT);
 }
